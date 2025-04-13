934c44d45215836a39a21d1fbe7db83024813d0f
lytask <1547717233@qq.com>
2025-04-14 06:42:13 +0800

    做了点优化

--- src/views/data/interviews.js
+++ src/views/data/interviews.js
@@ -7,7 +7,7 @@ export default [
         "聚焦教育教学场景！专业反馈答题逻辑，帮你夯实教师职业素养。",
       type: "structured",
       code: "TEACHER_QUALIFICATION_EXAM",
-      image: require("@/assets/avatar/TEACHER_QUALIFICATION_EXAM.png"),
+      image: "https://up2.aigcpmer.com/TEACHER_QUALIFICATION_EXAM.png",
       icon: "el-icon-user-solid",
       isForWaiting: false
     },
@@ -18,7 +18,7 @@ export default [
         "精准匹配教师岗位！分场景实战模拟，提升班级管理与综合素养应答能力。",
       type: "structured",
       code: "TEACHER_RECRUITMENT_EXAM",
-      image: require("@/assets/avatar/TEACHER_RECRUITMENT_EXAM.png"),
+      image: "https://up2.aigcpmer.com/TEACHER_RECRUITMENT_EXAM.png",
       icon: "el-icon-user-solid",
       isForWaiting: false
     },
@@ -29,7 +29,7 @@ export default [
         "培养政务思维！实时评估回答的拆解深度与对策可行性，练出“公务员式”精准应答！",
       type: "structured",
       code: "GOV_EXAM",
-      image: require("@/assets/avatar/GOV_EXAM.png"),
+      image: "https://up2.aigcpmer.com/GOV_EXAM.png",
       icon: "el-icon-user",
       isForWaiting: false
     },
@@ -39,7 +39,7 @@ export default [
       description: "强化岗位认知！帮你告别“外行思维”，用“岗位专家”视角征服考官！",
       type: "structured",
       code: "GOV_INTERVIEW",
-      image: require("@/assets/avatar/GOV_INTERVIEW.png"),
+      image: "https://up2.aigcpmer.com/GOV_INTERVIEW.png",
       icon: "el-icon-user-solid",
       isForWaiting: true
     },
@@ -50,7 +50,7 @@ export default [
         "题库由BAT等大厂以及顶尖AI创业公司的产品负责人参与编审，深度还原头部企业考核标准，覆盖产品设计、技术边界评估、商业化闭环等核心能力模型。",
       type: "internet",
       code: "AI_PRODUCT_MANAGER",
-      image: require("@/assets/avatar/AI_PRODUCT_MANAGER.png"),
+      image: "https://up2.aigcpmer.com/AI_PRODUCT_MANAGER.png",
       icon: "el-icon-user-solid",
       isForWaiting: true
     },
@@ -62,7 +62,7 @@ export default [
       type: "internet",
       code: "PRODUCT_OPERATION",
       icon: "el-icon-user-solid",
-      image: require("@/assets/avatar/PRODUCT_OPERATION.png"),
+      image: "https://up2.aigcpmer.com/PRODUCT_OPERATION.png",
       isForWaiting: true
     },
     {
@@ -72,7 +72,7 @@ export default [
         "对标头部大厂中高级面试标准，精选框架原理、浏览器渲染机制等高频考点，覆盖30+大厂终面真题原型。",
       type: "internet",
       code: "FRONTEND_DEVELOPMENT",
-      image: require("@/assets/avatar/FRONTEND_DEVELOPMENT.png"),
+      image: "https://up2.aigcpmer.com/FRONTEND_DEVELOPMENT.png",
       icon: "el-icon-user-solid",
       isForWaiting: true
     },
@@ -83,7 +83,7 @@ export default [
         "聚焦于数据库管理、服务器架构、API设计等领域，同时包括云计算、微服务等新兴领域的内容，汇集了一系列考察候选人技术深度和广度的问题。",
       type: "internet",
       code: "BACKEND_DEVELOPMENT",
-      image: require("@/assets/avatar/BACKEND_DEVELOPMENT.png"),
+      image: "https://up2.aigcpmer.com/BACKEND_DEVELOPMENT.png",
       icon: "el-icon-user-solid",
       isForWaiting: true
     },
@@ -94,9 +94,8 @@ export default [
         "聚焦数据驱动决策全链路能力培养，题库覆盖数据清洗、特征工程、统计建模等核心技能。",
       type: "structured",
       code: "DATA_SCIENCE",
-      image: require("@/assets/avatar/DATA_SCIENCE.png"),
+      image: "https://up2.aigcpmer.com/DATA_SCIENCE.png",
       icon: "el-icon-user-solid",
       isForWaiting: true
     }
   ];
-  
